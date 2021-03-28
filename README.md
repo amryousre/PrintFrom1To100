e59f2397be698e3c7deff9c29bce3a876d392785
Amr Yousre <master_j_20@hotmail.com>
2021-03-28 15:13:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PrintFrom1To100
+ PrintFrom1To100 And The Even Numbers only
---
+++ bin/PrintFrom1To100.dart
@@ -0,0 +1,17 @@
+void main() {
+for(int number= 0;number<=100;number++){                                   //loop to print from 1:100
+  if(number%10!=0){                                              //if condition to check if the number not 10,20,30 etc print it
+    print(number);
+  }
+}
+
+
+
+
+for (int number=0;number<100;number++){                                    //loop to print the even numbers only
+  if(number%2==0){
+    print(number);
+  }
+
+}
+}
